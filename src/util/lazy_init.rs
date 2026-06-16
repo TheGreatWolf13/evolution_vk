324ae204e7d6cb5e6ea9d21093c4f81e76676987
TheGreatWolf <16447647+thegreatwolf13@users.noreply.github.com>
2026-06-16 07:22:40 -0300

    Added LazyInit for common patterns.

---
+++ src/util/lazy_init.rs
@@ -0,0 +1,28 @@
+﻿pub struct LazyInit<'a, T> {
+    t: Option<T>,
+    t_maker: Option<Box<dyn FnOnce() -> T + 'a>>,
+}
+
+impl<'a, T> LazyInit<'a, T> {
+    pub fn new<F: FnOnce() -> T + 'a>(t_maker: F) -> Self {
+        Self {
+            t: None,
+            t_maker: Some(Box::new(t_maker)),
+        }
+    }
+
+    pub fn get_or_init(&mut self) -> &mut T {
+        let t = &mut self.t;
+        if let Some(t) = t {
+            t
+        } //
+        else {
+            *t = Some(self.t_maker.take().unwrap()());
+            t.as_mut().unwrap()
+        }
+    }
+
+    pub fn get(self) -> Option<T> {
+        self.t
+    }
+}
